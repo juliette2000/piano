c80d54e429bad80f07649e984147e3886791d350
juliette2000 <chen.juliette2000@gmail.com>
2020-09-09 20:39:12 -0400

    test

---
+++ piano.js
@@ -0,0 +1,9 @@
+function playNote(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    audio.currentTime = 0;
+    audio.play();
+    // const playKey= document.getElementById(`"${e.keyCode}"`);
+    // console.log(playKey);
+    playKey.style.backgroundColor="green";
+  }
+  window.addEventListener("keydown", playNote);
